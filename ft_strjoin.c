0a995730d925045fb636cb6a98d6e362a0d54dd9
Weglarz <lucasweglarz9@gmail.com>
2020-08-25 13:22:23 +0200

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -20,7 +20,7 @@ char		*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-	if (!(str = malloc(sizeof(char *) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+	if (!(str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
 		return (NULL);
 	while (s1[i])
 	{
